b71d682ebe264051c76e932f2f5c2d11ee0c60b4
szfn <szfn@local>
2011-09-04 11:49:25 +0200

    fixed early font setup mistakes

--- font.c
+++ font.c
@@ -1,5 +1,7 @@
 #include "font.h"
 
+#include <gdk/gdk.h>
+
 void teddy_font_init(teddy_font_t *font, FT_Library *library, const char *fontpattern) {
     int error;
     FcPattern *pat, *match;
@@ -7,6 +9,7 @@ void teddy_font_init(teddy_font_t *font, FT_Library *library, const char *fontpa
     FcChar8 *fontfile;
     double size;
     double text_size;
+    gdouble dpi = gdk_screen_get_resolution(gdk_screen_get_default());
 
     pat = FcNameParse((const FcChar8 *)fontpattern);
     FcConfigSubstitute(NULL, pat, FcMatchPattern);
@@ -28,15 +31,21 @@ void teddy_font_init(teddy_font_t *font, FT_Library *library, const char *fontpa
         size = 12.0;
     }
 
-    text_size = 96.0 / 72.0 * size;
-    
+    text_size = dpi / 72.0 * size;
+
     error = FT_New_Face(*library, (const char *)fontfile, 0, &(font->face));
     if (error) {
         printf("Error loading freetype font\n");
         exit(EXIT_FAILURE);
     }
 
-    font->cairoface = cairo_ft_font_face_create_for_ft_face(font->face, 0);
+    error = FT_Set_Char_Size(font->face, 0, size * 64, dpi, dpi);
+    if (error) {
+        printf("Error loading freetype font\n");
+        exit(EXIT_FAILURE);
+    }
+
+    font->cairoface = cairo_ft_font_face_create_for_ft_face(font->face, FT_LOAD_FORCE_AUTOHINT);
 
     cairo_matrix_init(&(font->font_size_matrix), text_size, 0, 0, text_size, 0, 0);
     cairo_matrix_init(&(font->font_ctm), 1, 0, 0, 1, 0, 0);
