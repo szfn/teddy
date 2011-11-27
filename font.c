ba1d6c1ac6d9b21c49535306a37a7cd49564b1d3
szfn <szfn@localhost>
2011-11-27 16:52:46 +0100

    ability to specify a path to a font directly instead of going through fontconfig

--- font.c
+++ font.c
@@ -1,39 +1,13 @@
 #include "font.h"
 
+
 #include <gdk/gdk.h>
 
-void teddy_font_init(teddy_font_t *font, FT_Library *library, const char *fontpattern) {
-	int error;
-	FcPattern *pat, *match;
-	FcResult result;
-	FcChar8 *fontfile;
-	double size;
-	double text_size;
+static void teddy_font_init_ex(teddy_font_t *font, FT_Library *library, const char *fontfile, double size) {
 	gdouble dpi = gdk_screen_get_resolution(gdk_screen_get_default());
+	double text_size = dpi / 72.0 * size;
 
-	pat = FcNameParse((const FcChar8 *)fontpattern);
-	FcConfigSubstitute(NULL, pat, FcMatchPattern);
-	FcDefaultSubstitute(pat);
-	match = FcFontMatch(NULL, pat, &result);
-
-	if (!match) {
-		printf("Couldn't resolve font pattern [%s], this shouldn't happen\n", fontpattern);
-		FcPatternDestroy(pat);
-		exit(EXIT_FAILURE);
-	}
-
-	if (FcPatternGetString(match, FC_FILE, 0, &fontfile) != FcResultMatch) {
-		printf("Font file not found [%s]\n", fontpattern);
-		exit(EXIT_FAILURE);
-	}
-
-	if (FcPatternGetDouble(match, FC_SIZE, 0, &size) != FcResultMatch) {
-		size = 12.0;
-	}
-
-	text_size = dpi / 72.0 * size;
-
-	error = FT_New_Face(*library, (const char *)fontfile, 0, &(font->face));
+	int error = FT_New_Face(*library, (const char *)fontfile, 0, &(font->face));
 	if (error) {
 		printf("Error loading freetype font\n");
 		exit(EXIT_FAILURE);
@@ -55,10 +29,64 @@ void teddy_font_init(teddy_font_t *font, FT_Library *library, const char *fontpa
 	cairo_font_options_set_hint_style(font->font_options, CAIRO_HINT_STYLE_SLIGHT);
 
 	font->cairofont = cairo_scaled_font_create(font->cairoface, &(font->font_size_matrix), &(font->font_ctm), font->font_options);
+}
+
+static void teddy_font_init_fontconfig_pattern(teddy_font_t *font, FT_Library *library, const char *fontpattern) {
+	FcResult result;
+
+	FcPattern *pat = FcNameParse((const FcChar8 *)fontpattern);
+	FcConfigSubstitute(NULL, pat, FcMatchPattern);
+	FcDefaultSubstitute(pat);
+	FcPattern *match = FcFontMatch(NULL, pat, &result);
+
+	if (!match) {
+		printf("Couldn't resolve font pattern [%s], this shouldn't happen\n", fontpattern);
+		FcPatternDestroy(pat);
+		exit(EXIT_FAILURE);
+	}
+
+	FcChar8 *fontfile;
+	if (FcPatternGetString(match, FC_FILE, 0, &fontfile) != FcResultMatch) {
+		printf("Font file not found [%s]\n", fontpattern);
+		exit(EXIT_FAILURE);
+	}
+
+	double size;
+	if (FcPatternGetDouble(match, FC_SIZE, 0, &size) != FcResultMatch) {
+		size = 12.0;
+	}
+
+	teddy_font_init_ex(font, library, (const char *)fontfile, size);
 
 	FcPatternDestroy(match);
 	FcPatternDestroy(pat);
- }
+}
+
+static void teddy_font_init_direct_path(teddy_font_t *font, FT_Library *library, const char *fontspec) {
+	char *saveptr;
+	char *fontspec_copy = strdup(fontspec);
+	char *fontpath = strtok_r(fontspec_copy, ":", &saveptr);
+	
+	if (fontpath == NULL) {
+		fprintf(stderr, "Bizzarre error interpreting the font specification\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	char *sizestr = strtok_r(NULL, ":", &saveptr);
+	double size = atof(sizestr);
+	
+	teddy_font_init_ex(font, library, fontpath, size);
+	
+	free(fontspec_copy);
+}
+
+void teddy_font_init(teddy_font_t *font, FT_Library *library, const char *fontpattern) {
+	if (fontpattern[0] == '/') {
+		teddy_font_init_direct_path(font, library, fontpattern);
+	} else {
+		teddy_font_init_fontconfig_pattern(font, library, fontpattern);
+	}
+}
 
 void teddy_font_free(teddy_font_t *font) {
 	cairo_scaled_font_destroy(font->cairofont);
