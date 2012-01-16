feb797dc05c2c9155b94ec3dd7f96d9f2e8bee92
szfn <szfn@localhost>
2012-01-16 17:22:09 +0100

    loading fontset for main font

--- font.c
+++ font.c
@@ -6,6 +6,7 @@
 
 FT_Library library;
 teddy_font_t main_font;
+teddy_fontset_t main_fonts;
 teddy_font_t posbox_font;
 
 static void teddy_font_init_ex(teddy_font_t *font,  const char *fontfile, double size, int face_index) {
@@ -36,23 +37,10 @@ static void teddy_font_init_ex(teddy_font_t *font,  const char *fontfile, double
 	font->cairofont = cairo_scaled_font_create(font->cairoface, &(font->font_size_matrix), &(font->font_ctm), font->font_options);
 }
 
-static void teddy_font_init_fontconfig_pattern(teddy_font_t *font, const char *fontpattern) {
-	FcResult result;
-
-	FcPattern *pat = FcNameParse((const FcChar8 *)fontpattern);
-	FcConfigSubstitute(NULL, pat, FcMatchPattern);
-	FcDefaultSubstitute(pat);
-	FcPattern *match = FcFontMatch(NULL, pat, &result);
-
-	if (!match) {
-		printf("Couldn't resolve font pattern [%s], this shouldn't happen\n", fontpattern);
-		FcPatternDestroy(pat);
-		exit(EXIT_FAILURE);
-	}
-
+static void fontconfig_init_from_pattern(FcPattern *match, teddy_font_t *font) {
 	FcChar8 *fontfile;
 	if (FcPatternGetString(match, FC_FILE, 0, &fontfile) != FcResultMatch) {
-		printf("Font file not found [%s]\n", fontpattern);
+		printf("Font file not found\n");
 		exit(EXIT_FAILURE);
 	}
 
@@ -67,6 +55,23 @@ static void teddy_font_init_fontconfig_pattern(teddy_font_t *font, const char *f
 	}
 
 	teddy_font_init_ex(font, (const char *)fontfile, size, index);
+}
+
+static void teddy_font_init_fontconfig_pattern(teddy_font_t *font, const char *fontpattern) {
+	FcResult result;
+
+	FcPattern *pat = FcNameParse((const FcChar8 *)fontpattern);
+	FcConfigSubstitute(NULL, pat, FcMatchPattern);
+	FcDefaultSubstitute(pat);
+	FcPattern *match = FcFontMatch(NULL, pat, &result);
+
+	if (!match) {
+		printf("Couldn't resolve font pattern [%s], this shouldn't happen\n", fontpattern);
+		FcPatternDestroy(pat);
+		exit(EXIT_FAILURE);
+	}
+
+	fontconfig_init_from_pattern(match, font);
 
 	FcPatternDestroy(match);
 	FcPatternDestroy(pat);
@@ -106,6 +111,91 @@ static void teddy_font_free(teddy_font_t *font) {
 	FT_Done_Face(font->face);
 }
 
+static void match_write_coverage(teddy_fontset_t *fontset, int idx, FcCharSet *cset, FcCharSet *accumulator) {
+	FcChar32 ucs4;
+	FcChar32 map[FC_CHARSET_MAP_SIZE];
+	FcChar32 next;
+
+	FcCharSet *charnews = FcCharSetSubtract(cset, accumulator);
+
+	for (ucs4 = FcCharSetFirstPage(charnews, map, &next); ucs4 != FC_CHARSET_DONE; ucs4 = FcCharSetNextPage(charnews, map, &next)) {
+		for (int i = 0; i < FC_CHARSET_MAP_SIZE; i++) {
+			if (!map[i]) continue;
+			for (int j = 0; j < 32; j++) {
+				if (!(map[i] & (1 << j))) continue;
+				uint32_t cur = ucs4 + i * 32 + j;
+				if (cur > 0xffff) continue;
+				fontset->map[cur] = idx;
+			}
+		}
+	}
+}
+
+static void fontset_init(const char *fontfile, teddy_fontset_t *fontset) {
+	fontset->count = 0;
+
+	for (int i = 0; i <= 0xffff; ++i) {
+		fontset->map[i] = 0xff;
+	}
+
+	FcResult result;
+
+	FcPattern *pat = FcNameParse((const FcChar8 *)fontfile);
+	FcConfigSubstitute(NULL, pat, FcMatchPattern);
+	FcDefaultSubstitute(pat);
+
+	FcFontSet *set = FcFontSort(NULL, pat, FcTrue, NULL, &result);
+
+	if (!set) {
+		printf("Couldn't resolve font pattern [%s], this shouldn't happen\n", fontfile);
+		FcPatternDestroy(pat);
+		exit(EXIT_FAILURE);
+	}
+
+	FcCharSet *accumulator = FcCharSetCreate();
+
+	if (!accumulator) {
+		perror("Out of memory");
+		exit(EXIT_FAILURE);
+	}
+
+	int acc_count = 0;
+
+	for (int i = 0; i < set->nfont; ++i) {
+		FcPattern *match = set->fonts[i];
+
+		FcCharSet *cset;
+
+		if (FcPatternGetCharSet(match, FC_CHARSET, 0, &cset) != FcResultMatch) continue;
+
+		FcChar32 c = FcCharSetSubtractCount(cset, accumulator);
+
+		int limit = 0;
+		if (acc_count > 0) {
+			limit = 128;
+		}
+		if (acc_count > 1024) {
+			limit = 512;
+		}
+
+		if (c > limit) {
+			match_write_coverage(fontset, fontset->count, cset, accumulator);
+
+			fontconfig_init_from_pattern(match, fontset->fonts+fontset->count);
+			++(fontset->count);
+
+			FcCharSetMerge(accumulator, cset, NULL);
+			acc_count += c;
+		}
+	}
+}
+
+static void fontset_free(teddy_fontset_t *fontset) {
+	for (int i = 0; i < fontset->count; ++i) {
+		teddy_font_free(fontset->fonts + i);
+	}
+}
+
 void teddy_font_real_init(void) {
 	int error = FT_Init_FreeType(&library);
 	if (error) {
@@ -115,9 +205,13 @@ void teddy_font_real_init(void) {
 
 	teddy_font_init(&main_font, config[CFG_MAIN_FONT].strval);
 	teddy_font_init(&posbox_font, config[CFG_POSBOX_FONT].strval);
+
+	fontset_init(config[CFG_MAIN_FONT].strval, &main_fonts);
 }
+
 
 void teddy_font_real_free(void) {
 	teddy_font_free(&main_font);
 	teddy_font_free(&posbox_font);
+	fontset_free(&main_fonts);
 }
--- font.h
+++ font.h
@@ -5,6 +5,8 @@
 #include <ft2build.h>
 #include FT_FREETYPE_H
 
+#include <stdint.h>
+
 typedef struct _teddy_font_t {
 	FT_Face face;
 	cairo_font_face_t *cairoface;
@@ -13,8 +15,15 @@ typedef struct _teddy_font_t {
 	cairo_font_options_t *font_options;
 } teddy_font_t;
 
+typedef struct _teddy_fontset_t {
+	teddy_font_t fonts[0xff];
+	uint8_t map[0xfffff+1];
+	int count;
+} teddy_fontset_t;
+
 extern FT_Library library;
 extern teddy_font_t main_font;
+extern teddy_fontset_t main_fonts;
 extern teddy_font_t posbox_font;
 
 void teddy_font_real_init(void);
