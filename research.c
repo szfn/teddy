d8bc8e62b151e6020d555acbcd3bae972d73e287
szfn <szfn@localhost>
2012-07-01 10:25:07 +0200

    changed s command

--- research.c
+++ research.c
@@ -16,6 +16,7 @@ GtkWidget *research_button_replace;
 editor_t *research_editor;
 regex_t research_regexp;
 const char *research_subst;
+bool research_line_limit;
 bool research_next_will_wrap_around;
 
 typedef enum _research_window_action_t {
@@ -24,9 +25,16 @@ typedef enum _research_window_action_t {
 	RESEARCH_STOP = 2,
 } research_window_action_t;
 
+enum research_mode_t {
+	RM_INTERACTIVE = 0,
+	RM_SELECT,
+	RM_TOSTART,
+	RM_TOEND
+};
+
 research_window_action_t research_window_results[] = { RESEARCH_REPLACE, RESEARCH_NEXT, RESEARCH_STOP };
 
-static void move_regexp_search_forward(bool start_at_top) {
+static void move_regexp_search_forward(bool start_at_top, enum research_mode_t research_mode) {
 #define OVECTOR_SIZE 10
 	regmatch_t ovector[OVECTOR_SIZE];
 
@@ -51,6 +59,10 @@ static void move_regexp_search_forward(bool start_at_top) {
 		int r = tre_reguexec(&research_regexp, &tss, OVECTOR_SIZE, ovector, 0);
 
 		if (r == REG_NOMATCH) {
+			if (research_line_limit) {
+				if (research_mode != RM_INTERACTIVE) Tcl_SetResult(interp, "false", TCL_VOLATILE);
+				return;
+			}
 			search_point.line = search_point.line->next;
 			search_point.start_glyph = 0;
 			search_point.offset = 0;
@@ -63,12 +75,30 @@ static void move_regexp_search_forward(bool start_at_top) {
 			int end_glyph = ovector[0].rm_eo + search_point.start_glyph;
 
 			if ((start_glyph >= 0) && (end_glyph >= 0)) {
-				research_editor->buffer->cursor.line = research_editor->buffer->mark.line = search_point.line;
-				research_editor->buffer->mark.glyph = start_glyph;
-				research_editor->buffer->cursor.glyph = end_glyph;
+				switch (research_mode) {
+				case RM_INTERACTIVE:
+				case RM_SELECT:
+					research_editor->buffer->cursor.line = research_editor->buffer->mark.line = search_point.line;
+					research_editor->buffer->mark.glyph = start_glyph;
+					research_editor->buffer->cursor.glyph = end_glyph;
+					break;
+				case RM_TOSTART:
+					research_editor->buffer->cursor.line = search_point.line;
+					research_editor->buffer->cursor.glyph = start_glyph;
+					break;
+				case RM_TOEND:
+					research_editor->buffer->cursor.line = search_point.line;
+					research_editor->buffer->cursor.glyph = end_glyph;
+					break;
+				}
+
 				lexy_update_for_move(research_editor->buffer, research_editor->buffer->cursor.line);
 				editor_center_on_cursor(research_editor);
 				gtk_widget_queue_draw(research_editor->drar);
+
+				if (research_mode != RM_INTERACTIVE) Tcl_SetResult(interp, "true", TCL_VOLATILE);
+			} else { // if we end up here it's probably a bug
+				if (research_mode != RM_INTERACTIVE) Tcl_SetResult(interp, "false", TCL_VOLATILE);
 			}
 			return;
 		}
@@ -76,9 +106,14 @@ static void move_regexp_search_forward(bool start_at_top) {
 
 	// if we get here no match was found and we are at the end of the buffer
 
+	if (research_mode != RM_INTERACTIVE) {
+		Tcl_SetResult(interp, "false", TCL_VOLATILE);
+		return;
+	}
+
 	if (research_next_will_wrap_around) {
 		research_next_will_wrap_around = false;
-		move_regexp_search_forward(true);
+		move_regexp_search_forward(true, RM_INTERACTIVE);
 	} else {
 		research_next_will_wrap_around = true;
 	}
@@ -95,7 +130,7 @@ static void research_replace_selection(void) {
 		//TODO: check that there is a selection active
 		editor_replace_selection(research_editor, research_subst);
 	}
-	move_regexp_search_forward(false);
+	move_regexp_search_forward(false, RM_INTERACTIVE);
 }
 
 static void research_perform_action(research_window_action_t *action) {
@@ -104,7 +139,7 @@ static void research_perform_action(research_window_action_t *action) {
 		research_replace_selection();
 		break;
 	case RESEARCH_NEXT:
-		move_regexp_search_forward(false);
+		move_regexp_search_forward(false, RM_INTERACTIVE);
 		break;
 	case RESEARCH_STOP:
 		research_stop_search();
@@ -123,7 +158,7 @@ static gboolean research_key_press_callback(GtkWidget *widget, GdkEventKey *even
 		research_replace_selection();
 		break;
 	case GDK_KEY_n:
-		move_regexp_search_forward(false);
+		move_regexp_search_forward(false, RM_INTERACTIVE);
 		break;
 	case GDK_KEY_Escape:
 	case GDK_KEY_s:
@@ -198,7 +233,7 @@ static char *automatic_search_and_replace(char *text, regex_t *re, const char *s
 	return r;
 }
 
-static void start_regexp_search(editor_t *editor, const char *regexp, const char *subst) {
+static void start_regexp_search(editor_t *editor, const char *regexp, const char *subst, bool line_limit, enum research_mode_t research_mode) {
 	int r = tre_regcomp(&research_regexp, regexp, REG_EXTENDED);
 
 	if (r != REG_OK) {
@@ -216,17 +251,15 @@ static void start_regexp_search(editor_t *editor, const char *regexp, const char
 	lpoint_t start, end;
 	buffer_get_selection(editor->buffer, &start, &end);
 
-	if ((start.line != NULL) && (end.line != NULL)) {
-		// there is an active selection, replace all occourence
-		if (subst != NULL) {
-			char *text = buffer_lines_to_text(editor->buffer, &start, &end);
-			char *newtext = automatic_search_and_replace(text, &research_regexp, subst);
+	if ((start.line != NULL) && (end.line != NULL) && (subst != NULL)) {
+		// there is an active selection and we specified a substitution, replace all occourence
+		char *text = buffer_lines_to_text(editor->buffer, &start, &end);
+		char *newtext = automatic_search_and_replace(text, &research_regexp, subst);
 
-			editor_replace_selection(editor, newtext);
+		editor_replace_selection(editor, newtext);
 
-			free(text);
-			free(newtext);
-		}
+		free(text);
+		free(newtext);
 
 		tre_regfree(&research_regexp);
 	} else {
@@ -234,16 +267,22 @@ static void start_regexp_search(editor_t *editor, const char *regexp, const char
 
 		research_editor = editor;
 		research_subst = subst;
+		research_line_limit = line_limit;
 		research_next_will_wrap_around = false;
 
-		gtk_widget_show_all(research_window);
-		gtk_widget_set_visible(research_button_replace, research_subst != NULL);
+		if (research_mode == RM_INTERACTIVE) {
+			gtk_widget_show_all(research_window);
+			gtk_widget_set_visible(research_button_replace, research_subst != NULL);
+		}
 
-		move_regexp_search_forward(false);
+		move_regexp_search_forward(false, research_mode);
 	}
 }
 
 int teddy_research_command(ClientData client_data, Tcl_Interp *interp, int argc, const char *argv[]) {
+	enum research_mode_t research_mode = RM_INTERACTIVE;
+	bool line_limit = false;
+
 	if (context_editor == NULL) {
 		Tcl_AddErrorInfo(interp, "No editor open, can not execute 's' command");
 		return TCL_ERROR;
@@ -258,6 +297,10 @@ int teddy_research_command(ClientData client_data, Tcl_Interp *interp, int argc,
 	for (i = 1; i < argc; ++i) {
 		if (argv[i][0] != '-') break;
 		if (strcmp(argv[i], "--") == 0) { ++i; break; }
+		if (strcmp(argv[i], "-0") == 0) { research_mode = RM_TOSTART; }
+		if (strcmp(argv[i], "-$") == 0) { research_mode = RM_TOEND; }
+		if (strcmp(argv[i], "-sel") == 0) { research_mode = RM_SELECT; }
+		if (strcmp(argv[i], "-line") == 0) { line_limit = true; }
 	}
 
 	if ((i >= argc) || (i+2 < argc)) {
@@ -272,7 +315,7 @@ int teddy_research_command(ClientData client_data, Tcl_Interp *interp, int argc,
 		subst = argv[i+1];
 	}
 
-	start_regexp_search(context_editor, regexp, subst);
+	start_regexp_search(context_editor, regexp, subst, line_limit, research_mode);
 
 	return TCL_OK;
 }
